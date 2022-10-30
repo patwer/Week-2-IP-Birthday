1e8106b55c2fe008c730517ae63d500cdebf9d6c
Patwer <patweru@gmail.com>
2022-10-30 20:03:00 +0300

    if statment implmented

--- birthday.js
+++ birthday.js
@@ -1,24 +1,28 @@
-const empty_string = (input) => {
-    console.log('Input field')
-    return input;
-}
-
-
+//function to pick the date from input date of birth and map to Akan name
 const calendar_dates=()=>
 {
 //get values from form elements
+//gender
+const elementGender=document.getElementById('gender');
+const gender=String(elementGender.value);
+console.log(gender);
+
+//first name
 const elementFirstName=document.getElementById('fname');
 const fname=String(elementFirstName.value);
 console.log(fname);
 
+//second name
 const elementLastName=document.getElementById('lname');
 const lname=String(elementLastName.value);
 console.log(lname);
 
+//Title
 const elementTitle=document.getElementById('title');
 const title=String(elementTitle.value);
 console.log(title);
 
+//Date of birth
 const elementDOB=document.getElementById('dobirth');
 const dobirth=String(elementDOB.value);
 console.log(dobirth);
@@ -46,74 +50,42 @@ console.log(year_digit);
 //CC - is the century digits. For example 1989 has CC = 19, YY - is the Year digits (1989 has YY = 89), MM -  is the Month
 //DD - is the Day of the month and mod - is the modulus function ( % )
 const d_o_week=( ( (century/4) -2*century-1) + ((5*year_digit/4) ) + ((26*(month+1)/10)) + calendar_day )%7;
-//const d_o_week=( ( (century/4) -2*century-1) + ((5*year_digit/4) ) + ((26*(month+1)/10)) + calendar_day );
-//const modnum=16%7;
-//to pick the first digit only
-day_o_week=Math.trunc(d_o_week);
-//console.log(rem_int);
-console.log(day_o_week);
-//instantiate array
 
-//check for day of week
+//to pick the int part only
+day_o_week=Math.trunc(d_o_week);
+
+
+console.log(day_o_week);
+
+
+//check for day of week from array
 const array_day_of_week=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 const day_of_the_week=array_day_of_week[day_o_week];
 console.log(day_of_the_week);
+
+if (gender=="male")
+{
 //if male
 const array_male_names=['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
 const myMaleName=array_male_names[day_o_week];
 console.log(myMaleName);
+window.alert(`As you were born on a ${day_of_the_week} your Akan name is ${myMaleName}`);
+}
+else if(gender=="female")
+{
 //if female
 const array_female_names=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
 const myFemaleName=array_female_names[day_o_week];
 console.log(myFemaleName);
+window.alert(`As you were born on a ${day_of_the_week} your Akan name is ${myFemaleName}`);
+}
+
 
 //response from function
 //return(
     //window.alert(dobirth);
    // window.alert(`As you were born on a${dobirth}`);
-   window.alert(`As you were born on a ${day_of_the_week} your Akan name is ${lname}`);
+  // window.alert(`As you were born on a ${day_of_the_week} your Akan name is ${lname}`);
 //window.alert("As you were born on a"+dobirth+"your Akan name is" lname);
 
 }
-
-
-
-const day_of_week = (dateofbirth)=>{
-  
-    //ask user for date of birth, pick dob from input
-   // const dateofbirth=prompt('Please enter your dob');
-   // const dateofbirth=document.getElementById(dobirth);
-    console.log(dateofbirth);
-    //dateofbirth array split based on /
-    const dobArray=String(dateofbirth).split("/");
-    //index0
-    const calendar_day_o_week=dobArray[0];
-    //index1
-    const day_of_month=dobArray[1];
-   //index2
-   const year_of_birth=dobArray[2];
-}
-
-//day_of_week(dateofbirth)
-
-//const array_day_of_week=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-//const array_male_names=['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
-//const array_female_names=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
-
-const akan_name = (calendar_day_o_week)=>{
-// const akan_name = (calendar_day_o_week)=>{
-    const array_day_of_week=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-const array_male_names=['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
-const array_female_names=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
-    
-    const day_name=array_day_of_week[calendar_day_o_week];
-
-    const myAkanNameM=array_male_names[calendar_day_o_week];
-    const myAkanNameF=array_female_names[calendar_day_o_week];
-    console.log("my akan name", myAkanNameF);
-    console.log("my akan name", myAkanNameM);
-    return myAkanNameF;
-    return myAkanNameM;
-}
-
-akan_name(2)
