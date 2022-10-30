f0b84b37a979552777c2360690821e7a63d8629a
Patwer <patweru@gmail.com>
2022-10-30 14:29:18 +0300

    updated js to pick input from form

--- birthday.js
+++ birthday.js
@@ -4,14 +4,41 @@ const empty_string = (input) => {
 }
 
 
+const calendar_dates=()=>
+{
+//get values from form elements
+const elementFirstName=document.getElementById('fname');
+const fname=String(elementFirstName.value);
+console.log(fname);
+
+const elementLastName=document.getElementById('lname');
+const lname=String(elementLastName.value);
+console.log(lname);
+
+const elementTitle=document.getElementById('title');
+const title=String(elementTitle.value);
+console.log(title);
+
+const elementDOB=document.getElementById('dobirth');
+const dobirth=String(elementDOB.value);
+console.log(dobirth);
+//instantiate array
+
+//response from function
+
+
+}
 
 
 
 const day_of_week = (dateofbirth)=>{
-    //pick dob from input
-    const dob=dateofbirth;
+  
+    //ask user for date of birth, pick dob from input
+   // const dateofbirth=prompt('Please enter your dob');
+   // const dateofbirth=document.getElementById(dobirth);
+    console.log(dateofbirth);
     //dateofbirth array split based on /
-    const dobArray=String(dob).split("/");
+    const dobArray=String(dateofbirth).split("/");
     //index0
     const calendar_day_o_week=dobArray[0];
     //index1
@@ -20,11 +47,14 @@ const day_of_week = (dateofbirth)=>{
    const year_of_birth=dobArray[2];
 }
 
+//day_of_week(dateofbirth)
+
 //const array_day_of_week=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 //const array_male_names=['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
 //const array_female_names=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
 
 const akan_name = (calendar_day_o_week)=>{
+// const akan_name = (calendar_day_o_week)=>{
     const array_day_of_week=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 const array_male_names=['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
 const array_female_names=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
